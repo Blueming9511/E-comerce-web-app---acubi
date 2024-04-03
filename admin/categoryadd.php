6ef5864f5a3519fa733ed40576e93fb5bc06f9ce
janhae04 <hlchy11997@gmail.com>
2024-04-04 00:08:34 +0700

    Add more backend

---
+++ admin/categoryadd.php
@@ -0,0 +1,17 @@
+<?php
+include "header.php";
+include "sidebar.php";
+?>
+
+<div class="right">
+      <div class="category add">
+        <h1>Thêm sản phẩm</h1>
+        <form action="" method="post">
+          <input type="text" name="" id="" placeholder="Nhập tên sản phẩm">
+          <button type="submit">Thêm</button>
+        </form>
+      </div>
+    </div>
+  </section>
+</body>
+</html>
---
+++ admin/sidebar.php
@@ -0,0 +1,16 @@
+<section class="admin-content">
+    <div class="left">
+    
+      <ul> 
+        <li><a href="">Loại sản phẩm</a></li>
+          <ul>
+            <li><a href="">Thêm loại sản phẩm</a></li>
+            <li><a href="">Danh sách loại sản phẩm</a></li>
+          </ul>
+        <li><a href="">Sản phẩm</a></li>
+        <ul>
+          <li><a href="">Thêm sản phẩm</a></li>
+          <li><a href="">Danh sách sản phẩm</a></li>
+        </ul>
+      </ul>
+    </div>
