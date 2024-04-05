e8fefe40eb4ace20f86e8f5bef54f2ae381201d6
Kay <109297172+Blueming9511@users.noreply.github.com>
2024-04-05 15:44:30 +0700

    update progresses

--- asset/js/addToCart.js
+++ asset/js/addToCart.js
@@ -1,39 +1,57 @@
 const products = [
-    {
-      id: 1,
-      name: 'product 1',
-      price: 10,
-      img: 'asset/image/SHOP-IMG/i1.jpg'
-    },
-    {
-      id: 2,
-      name: 'product 2',
-      price: 20,
-      img: 'asset/image/SHOP-IMG/i2.jpg'
-    },
-    {
-      id: 3,
-      name: 'product 3',
-      price: 30,
-      img: 'asset/image/SHOP-IMG/i3.jpg'
-    },
-    {
-      id: 4,
-      name: 'product 4',
-      price: 40,
-      img: 'asset/image/SHOP-IMG/i4.jpg'
-    }
-  ];
-  
-  let product_list = document.querySelector(".bestSeller-shop-field");
-  
-  function initApp () {
-    products.forEach((value, key) => {
-      let newItem = document.createElement('div');
-      newItem.innerHTML = `
-          <div class="item-img">
-              <img src="${value.img}" alt="Tranding">
-          </div>
+  {
+    id: 1,
+    name: 'product 1',
+    price: 10,
+    img: 'asset/image/SHOP-IMG/m1.jpg',
+    category: 'bottom',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, dolorem.'
+  },
+  {
+    id: 2,
+    name: 'product 2',
+    price: 20,
+    img: 'asset/image/SHOP-IMG/i2.jpg',
+    category: 'bottom',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, dolorem.'
+  },
+  {
+    id: 3,
+    name: 'product 3',
+    price: 30,
+    img: 'asset/image/SHOP-IMG/i3.jpg',
+    category: 'bottom',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, dolorem.'
+  },
+  {
+    id: 4,
+    name: 'product 4',
+    price: 40,
+    img: 'asset/image/SHOP-IMG/i4.jpg',
+    category: 'top',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, dolorem.'
+  }
+]
+
+let product_list = document.querySelector('.shop-field')
+
+function initApp () {
+    
+  products.forEach((value, key) => {
+    let newItem = document.createElement('div')
+    newItem.innerHTML = `
+      <div class="image">
+				<div id="zoom-In">
+					<figure>
+                    <img src="${value.img}" alt="Tranding">
+					</figure>
+				</div>
+			</div>
+        
   
           <div class="item-content">
               <p class="item-name">
@@ -50,40 +68,41 @@ const products = [
                   </svg>
               </p>
           </div>
-      `;
-      newItem.classList.add("item");
-      product_list.appendChild(newItem);
-    });
+      `
+        newItem.classList.add('item')
+        newItem.setAttribute('data-name', value.id)
+        product_list.appendChild(newItem)
+    })
+}
+
+initApp()
+let total = document.querySelector('.total-money')
+let quantity = document.querySelector('.items-amount')
+
+let listCards = []
+let listCard = document.querySelector('.cart-body')
+
+function addToCart (key) {
+  if (listCards[key] == null) {
+    listCards[key] = products[key]
+    listCards[key].quantity = 1
+  } else {
+    listCards[key].quantity++
   }
-  
-  initApp();
-  let total = document.querySelector(".total-money");
-  let quantity = document.querySelector(".items-amount");
-  
-  let listCards = []
-  let listCard = document.querySelector(".cart-body");
-  
-  function addToCart(key){
-      if(listCards[key] == null){
-          listCards[key] = products[key];
-          listCards[key].quantity = 1;
-      } else {
-          listCards[key].quantity++;
-      }
-      
-      reloadCard();
-  }
-  
-  function reloadCard(){
-      listCard.innerHTML = '';
-      let count = 0;
-      let totalPrice = 0;
-      listCards.forEach((value, key) =>{
-          totalPrice += value.price * value.quantity;
-          count += value.quantity;
-          if(value != null){
-              let newDiv = document.createElement('li');
-              newDiv.innerHTML = `
+
+  reloadCard()
+}
+
+function reloadCard () {
+  listCard.innerHTML = ''
+  let count = 0
+  let totalPrice = 0
+  listCards.forEach((value, key) => {
+    totalPrice += value.price * value.quantity
+    count += value.quantity
+    if (value != null) {
+      let newDiv = document.createElement('li')
+      newDiv.innerHTML = `
                   <div class="image">
                       <img src="${value.img}" alt="">
                   </div>
@@ -94,28 +113,70 @@ const products = [
                       $ ${value.price}
                   </div>
                   <div class="Quantity">
-                      <span onclick="changeQuantity(${key}, ${value.quantity - 1})">-</span>
+                      <span onclick="changeQuantity(${key}, ${
+        value.quantity - 1
+      })">-</span>
                       <span>${value.quantity}</span>
-                      <span onclick="changeQuantity(${key}, ${value.quantity + 1})">+</span>
+                      <span onclick="changeQuantity(${key}, ${
+        value.quantity + 1
+      })">+</span>
                   </div>
-              `;
-              newDiv.classList.add("cart-item");
-              listCard.appendChild(newDiv);
-          }
-      });
-  
-      total.innerText = totalPrice;
-      quantity.innerText = count;
+              `
+      newDiv.classList.add('cart-item')
+      listCard.appendChild(newDiv)
+    }
+  })
+
+  total.innerText = totalPrice
+  quantity.innerText = count
+}
+
+function changeQuantity (key, quantity) {
+  if (quantity <= 0) {
+    delete listCards[key]
+  } else {
+    listCards[key].quantity = quantity
   }
-  
-  function changeQuantity(key, quantity){
-      if(quantity <= 0){
-          delete listCards[key];
-      } else {
-          listCards[key].quantity = quantity;
+
+  reloadCard()
+}
+
+{
+  /* <div class="item-img">
+             
+</div> */
+}
+let preveiwContainer = document.querySelector('.products-preview')
+let previewBox = preveiwContainer.querySelectorAll('.preview')
+
+document.querySelectorAll('.shop-field .item').forEach(product => {
+  product.onclick = () => {
+    preveiwContainer.style.display = 'flex';
+    let name = product.getAttribute('data-name');
+    console.log(name);
+    previewBox.forEach(preview => {
+      let target = preview.getAttribute('data-target')
+      if (name == target) {
+        preview.classList.add('active')
       }
-      
-      reloadCard();
-  } 
-  
+    })
+  }
+})
+
+previewBox.forEach(close => {
+  close.querySelector('.fa-times').onclick = () => {
+    let name = close.getAttribute('data-target');
+
+
+    previewBox.forEach(preview => {
+      let target = preview.getAttribute('data-target')
+      if (name == target) {
+        preview.classList.remove('active')
+      }
+    })
+    preveiwContainer.style.display = 'none'
+  }
+})
+
+
 
