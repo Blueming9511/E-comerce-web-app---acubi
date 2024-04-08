436f3e3298ae93aa36edc35c03350df23bd93a37
janhae4 <hlchy11997@gmail.com>
2024-04-08 15:17:57 +0700

    .

--- productlinkgenerate.php
+++ productlinkgenerate.php
@@ -1,26 +1,10 @@
 <?php
 include 'class/shop_class.php';
-
-// Check if the productlinks.xml file exists
-$productLinksFile = 'productlinks.xml';
-if (file_exists($productLinksFile)) {
-    // Load the existing XML file
-    $xml = simplexml_load_file($productLinksFile);
-    if ($xml === false) {
-        // Handle XML loading error
-        die('Error loading XML file: ' . libxml_get_last_error()->message);
-    }
-} else {
-    // Create a new XML structure if the file doesn't exist
-    $xml = new SimpleXMLElement('<products></products>');
-}
-
+$xml = new SimpleXMLElement('<products></products>');
 $products = new Shop;
 $showProducts = $products->displayProducts();
-var_dump($showProducts);
 if ($showProducts) {
     while ($result = $showProducts->fetch_assoc()) {
-        var_dump($result);
         // Add a new <product> element for each product
         $element = $xml->addChild('product');
         $linkImg = "admin/uploads/" . $result['product_img'];
@@ -32,8 +16,5 @@ if ($showProducts) {
 }
 
 // Save the modified XML data to the productlinks.xml file
-if ($xml->asXML($productLinksFile) === false) {
-    // Handle XML saving error
-    die('Error saving XML file');
-}
+$xml->asXML('productlinks.xml');
 ?>
